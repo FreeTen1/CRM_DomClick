aef672bc0bddb0d4a3922f34bc3a54762de9057b
Misha <mishatambov@gmail.com>
2021-07-23 00:56:53 +0300

    added information output

--- CRM/urls.py
+++ CRM/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from CRM.views import IndexView, WorkerList
+from CRM.views import IndexView, WorkerList, ClientList, QueryList
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('workers', WorkerList.as_view()),
+    path('clients', ClientList.as_view()),
+    path('queries', QueryList.as_view()),
 ]
--- CRM/views.py
+++ CRM/views.py
@@ -11,6 +11,20 @@ class IndexView(TemplateView):
 
 class WorkerList(ListView):
     model = Worker
+    queryset = Worker.objects.all()
     template_name = 'worker_list.html'
     context_object_name = 'workers'
-    queryset = Worker.objects.all()
+
+
+class ClientList(ListView):
+    model = Client
+    queryset = Client.objects.all()
+    template_name = 'client_list.html'
+    context_object_name = 'clients'
+
+
+class QueryList(ListView):
+    model = Query
+    queryset = Query.objects.all()
+    template_name = 'query_list.html'
+    context_object_name = 'queries'

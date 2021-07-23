0a8fdcaca4b92940e61763f30ba99f681a3b9fe2
Misha <mishatambov@gmail.com>
2021-07-23 19:49:14 +0300

    add test
    Не знаю какие тесты

--- CRM/tests.py
+++ CRM/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Client, Worker, Query
+
+
+class CemTestCases(TestCase):
+    def setUp(self) -> None:
+        self.client = Client.objects.create(name='Андрей', last_name='Прист', email='kjk@gmail.com')
+        self.worker = Worker.objects.create(name='Юлий', last_name='Гай', email='cezar@gmail.com', position='Простой рабочий')
+        self.query = Query.objects.create(client=self.client, type='RE', status='OP')
